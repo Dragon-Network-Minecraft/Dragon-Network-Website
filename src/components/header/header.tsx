947830ade0322ec06204926460cbd814bb448b12
G9 Aerospace <g9militants@gmail.com>
2024-02-24 20:37:04 +0530

    🛠️ header.module.css -> Adjusted padding and maximum width, added logo animation 🛠️ header.module.css -> Changed background color and adjusted font size and gap 🛠️ header.tsx -> Updated logo image and added animated background class

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,16 @@
 import { component$ } from "@builder.io/qwik";
 import styles from "./header.module.css";
-import dragonIcon from "../images/dragonnetwork-transparent.png";
 
 export default component$(() => {
+  const aquaLogoUrl = "https://media.discordapp.net/attachments/1134051261688328294/1210964439512121365/aqualogo-removebg-preview.png?ex=65ec7985&is=65da0485&hm=035db77170dd86412fbf4672147696c371cec79f0d46f2cd1668659b84f89e37&=&format=webp&quality=lossless&width=512&height=512";
+
   return (
-    <header class={styles.header}>
+    <header class={`${styles.header} ${styles.animatedBackground}`}>
       <div class={styles.wrapper}>
         <div class={styles.logo}>
-          <img src={dragonIcon} alt="Dragon Icon" class={styles.icon} />
-          <a>
-            Dragon Network
-          </a>
+          <img src={aquaLogoUrl} alt="Aqua Logo" class={styles.animatedLogo} />
         </div>
-        <ul>
+        <ul class={styles.navList}>
           <li><a href="/">Home</a></li>
           <li><a href="/vote">Vote</a></li>
           <li><a href="/store">Store</a></li>
